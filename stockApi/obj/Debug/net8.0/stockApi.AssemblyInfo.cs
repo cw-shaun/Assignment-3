a5e3be0ff559b25d8b88f286a22dda31daa8d15e
cw-shaun <shaun@carwale.com>
2024-09-12 09:21:24 +0530

    Small Changes

--- stockApi/obj/Debug/net8.0/stockApi.AssemblyInfo.cs
+++ stockApi/obj/Debug/net8.0/stockApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stockApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47d1b3941d7c96619869ae17414309d0ad24717")]
 [assembly: System.Reflection.AssemblyProductAttribute("stockApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stockApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
